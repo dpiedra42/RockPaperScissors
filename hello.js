103b73009fd3492fa6a3206d39b5cf2a7afacc2e
deanna <42.dpiedra@gmail.com>
2021-07-01 18:17:48 +0200

    started one round of play

--- hello.js
+++ hello.js
@@ -1,22 +1,32 @@
 let computerMove;
+let playerMove;
 
 function computerPlay() {
     computerSelection = Math.floor(Math.random()*3);
-    console.log(computerSelection);
     switch (true) {
         case (computerSelection == 0):
             computerMove = "ROCK";
-            console.log("Computer chose rock");
             break;
         case (computerSelection == 1):
             computerMove = "PAPER";
-            console.log("Computer chose paper");
             break;
         case (computerSelection == 2):
             computerMove = "SCISSORS";
-            console.log("Computer chose scissors");
             break;
     }
 }
 
-computerPlay();
+function playRound(computerSelection, playerSelection) {
+    computerPlay();
+    playerMove = prompt("Choose Your Fighter: Rock, Paper, or Scissors!");
+    playerMove = playerMove.toUpperCase();
+
+    if (computerMove == playerMove)
+        console.log("No Winner! It's a tie, try again.");
+    else if (){
+        
+    }
+}
+
+playRound(computerMove, playerMove);
+
