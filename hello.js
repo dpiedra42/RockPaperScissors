ce76d03692a815c4603aab1d4bbca969034c02d4
deanna <42.dpiedra@gmail.com>
2021-07-02 13:16:07 +0200

    5 rounds done but ties count as a round

--- hello.js
+++ hello.js
@@ -1,32 +1,56 @@
 let computerMove;
 let playerMove;
+let computerScore = 0;
+let playerScore = 0;
 
 function computerPlay() {
     computerSelection = Math.floor(Math.random()*3);
-    switch (true) {
-        case (computerSelection == 0):
-            computerMove = "ROCK";
+    switch (computerSelection) {
+        case (0):
+            computerMove = "rock";
             break;
-        case (computerSelection == 1):
-            computerMove = "PAPER";
+        case (1):
+            computerMove = "paper";
             break;
-        case (computerSelection == 2):
-            computerMove = "SCISSORS";
+        case (2):
+            computerMove = "scissors";
             break;
     }
 }
 
-function playRound(computerSelection, playerSelection) {
+function playRound() {
     computerPlay();
     playerMove = prompt("Choose Your Fighter: Rock, Paper, or Scissors!");
-    playerMove = playerMove.toUpperCase();
+    playerMove = playerMove.toLowerCase();
 
     if (computerMove == playerMove)
-        console.log("No Winner! It's a tie, try again.");
-    else if (){
-        
+        console.log("No Winner! It's a tie, try again :)");
+    else if ((computerMove == "rock" && playerMove == "scissors") ||
+             (computerMove == "paper" && playerMove == "rock") ||
+             (computerMove == "scissors" && playerMove == "paper")) {
+        computerScore++;
+        console.log("Computer wins! " + computerMove + " beats " + playerMove);
     }
+    else if ((playerMove == "rock" && computerMove == "scissors") ||
+             (playerMove == "paper" && computerMove == "rock") ||
+             (playerMove == "scissors" && computerMove == "paper")) {
+        playerScore++;
+        console.log("You won! " + playerMove + " beats " + computerMove);
+             }
+    else 
+        console.log("you didn't enter a fighter!");
 }
 
-playRound(computerMove, playerMove);
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    if (computerScore > playerScore)
+        console.log("computer won with a score of " + computerScore + "!");
+    else if (playerScore > computerScore)
+        console.log("you won with a score of " + playerScore + "!");
+    else
+        console.log("no winner!");
+}
 
+game();
