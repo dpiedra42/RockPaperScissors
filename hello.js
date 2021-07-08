ddffae9583bef933b1287c6cada139dcbee5ad66
deanna <42.dpiedra@gmail.com>
2021-07-08 12:11:13 +0200

    working image display of computers choice

--- hello.js
+++ hello.js
@@ -5,6 +5,30 @@ let computerScore = 0;
 let playerScore = 0;
 const buttons = document.querySelectorAll('.buttons');
 
+function putImage() {
+    var img = document.createElement("img");
+
+    if (computerMove == 'rock') {
+        img.src = "https://freepikpsd.com/media/2019/10/cartoon-rock-png-3-Transparent-Images.png";
+        img.width = 80;
+        img.height = 80;
+        img.alt = "rock";
+    }
+    else if (computerMove == 'paper') {
+        img.src = "https://freepikpsd.com/media/2019/10/cartoon-paper-png-7-Transparent-Images.png";
+        img.width = 80;
+        img.height = 80;
+        img.alt = "paper";
+    }
+    else if (computerMove == 'scissors') {
+        img.src = "https://i.pinimg.com/originals/de/d7/34/ded734058afb08d065830124d3d97a54.png";
+        img.width = 80;
+        img.height = 80;
+        img.alt = "scissors";
+    }
+    document.getElementById("compSel").appendChild(img);
+}
+
 function computerPlay() {
     computerSelection = Math.floor(Math.random()*3);
     switch (computerSelection) {
@@ -22,8 +46,9 @@ function computerPlay() {
 
 function playRound() {
     computerPlay();
+    document.getElementById("compSel").innerHTML = "";
+    putImage();
 
-    document.getElementById("compSel").innerHTML = computerMove.toUpperCase();
     if (computerMove == playerMove) {
         document.getElementById("roundResult").innerHTML = "No Winner! It's a tie, try again :)";
         round--;
@@ -49,7 +74,7 @@ function game() {
 
     round++;
     document.getElementById("roundNumber").innerHTML = "Round: " + round;
-    document.getElementById("tryAgain").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
+    document.getElementById("scoreBoard").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
     if (round == 5){
         if (computerScore > playerScore)
             document.getElementById("roundResult").innerHTML = "Computer won with a score of " + computerScore + "!";
@@ -57,7 +82,7 @@ function game() {
             document.getElementById("roundResult").innerHTML = "You won with a score of " + playerScore + "!";
         else
             document.getElementById("roundResult").innerHTML = "No winner!";
-        // document.getElementById("tryAgain").innerHTML = "If you want to play again refresh the page :)";
+        // document.getElementById("scoreBoard").innerHTML = "If you want to play again refresh the page :)";
     }
 }
 
